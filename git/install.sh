284043e8547677d23f31f14db95639841693b5f7
Ed Langley <ed@edlangley.co.uk>
2019-05-27 20:42:03 +0100

    Fix bin paths in get config setup script

--- git/install.sh
+++ git/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 git config --global merge.tool "meld3"
-git config --global mergetool.meld3.cmd '$HOME/bin/gitmeld3merge.sh $BASE $LOCAL $REMOTE $MERGED'
-git config --global mergetool.diffuse3.cmd '$HOME/bin/gitdiffuse3merge.sh $BASE $LOCAL $REMOTE $MERGED'
+git config --global mergetool.meld3.cmd '$HOME/.dotfiles/bin/gitmeld3merge.sh $BASE $LOCAL $REMOTE $MERGED'
+git config --global mergetool.diffuse3.cmd '$HOME/.dotfiles/bin/gitdiffuse3merge.sh $BASE $LOCAL $REMOTE $MERGED'
 
