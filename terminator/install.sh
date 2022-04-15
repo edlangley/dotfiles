609c2654dfac21c67ec42ab869010dff3323bfea
Ed Langley <ed@edlangley.co.uk>
2022-04-15 12:58:30 +0100

    Add custom terminator desktop menu entry to launch with layout selector

--- terminator/install.sh
+++ terminator/install.sh
@@ -2,4 +2,5 @@
 
 mkdir -p ~/.config/terminator/
 cp ~/.dotfiles/terminator/config.default_and_embedded_dev_layout ~/.config/terminator/config
+cp ~/.dotfiles/terminator/terminator.desktop ~/.local/share/applications/
 
